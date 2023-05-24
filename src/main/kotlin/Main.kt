5d386c3bab95634415d8130b4b7caebff1d1ba3d
Kpacg <drakon40000@gmail.com>
2023-05-24 17:50:16 +0300

    HELLLLLO2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 fun main()
 {
     println("Privet!")
-    var Me = Player("Гриша", 1, 1500)
-    var Julia = Player("Julia", 2, 1600)
+    var Me = Player("Гриша", 1, 2500)
+    var Julia = Player("Julia", 2, 1000)
     Me.resfreshRating(Julia, 20)
     println(Me.playerELO)
 }
--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,9 +1,8 @@
 class Player constructor(var playerName: String, val playerID: Int, var playerELO: Int = 1500){
-    var playerCoefficient = 5
+    var playerCoefficient = 2.5
     fun resfreshRating(opponent: Player, result:Int)
     {
-        playerELO = playerELO + playerCoefficient*(
-                result - (20/(1 - Math.pow(10.0, ((opponent.playerELO - playerELO).toDouble()/400)))).toInt())
+        playerELO += (playerCoefficient * (result - (20 / (1 + Math.pow(10.0, ((opponent.playerELO - playerELO).toDouble() / 400)))))).toInt()
     }
     //var playerTeam
 }
