2801b6ec61c15ab8f937c520e161e90ce19876ef
Kpacg <drakon40000@gmail.com>
2023-05-24 17:29:35 +0300

    HELLLLLO2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main()
+{
+    println("Privet!")
+    var Me = Player("Гриша", 1, 1500)
+    var Julia = Player("Julia", 2, 1600)
+    Me.resfreshRating(Julia, 20)
+    println(Me.playerELO)
 }
